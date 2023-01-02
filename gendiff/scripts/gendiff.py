dd9d832d968329668a49a7f057e80e27af95f1d5
Daniil <danokp@mail.ru>
2023-01-02 14:46:28 +0700

    Add gendiff package

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    inputs = parse_args()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    main()
