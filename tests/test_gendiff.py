4c27ff0503741759e746ff24c5a8b35e40d4a92d
Daniil <danokp@mail.ru>
2023-01-07 00:57:51 +0700

    Add tests for yaml-files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,12 +2,27 @@ from gendiff.gendiff_func import generate_diff
 import os
 
 
+def write_answer_from_file(path):
+    with open(path) as file:
+        return ''.join(file.readlines())
+
 DIR_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
 path_json_file1 = os.path.join(DIR_PATH, 'file1.json')
 path_json_file2 = os.path.join(DIR_PATH, 'file2.json')
-with open(DIR_PATH+'/answer_json_file1_file2.txt') as file:
-    answer_json_file1_file2 = ''.join(file.readlines())
+answer_file1_file2 = write_answer_from_file(DIR_PATH+'/answer_json_file1_file2.txt')
+answer_help_option = write_answer_from_file(DIR_PATH+'/answer_help_option.txt')
+path_yaml_file1 = os.path.join(DIR_PATH, 'file1.yaml')
+path_yaml_file2 = os.path.join(DIR_PATH, 'file2.yaml')
 
 
-def test_gendiff():
-    assert generate_diff(path_json_file1, path_json_file2) == answer_json_file1_file2
+
+def test_gendiff_json():
+    assert generate_diff(path_json_file1, path_json_file2) == answer_file1_file2
+
+
+def test_gendiff_yaml():
+    assert generate_diff(path_yaml_file1, path_yaml_file2) == answer_file1_file2
+
+
+# def test_gendiff_options():
+#     assert == answer_help_option
