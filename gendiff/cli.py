3325633f1859fda65f0e77798ab50db394184662
Daniil <danokp@mail.ru>
2023-01-07 01:00:20 +0700

    Add cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,25 @@
+"""Make positional and optional arguments."""
+import argparse
+
+
+def parse_args():
+    """Return the files to compare.
+
+    Create command-line options (-h (--help), -f (--format))
+    and arguments (first_file, second_file).
+
+    Returns:
+        Namespace with files to compare and their format.
+    """
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.',
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        help='set format of output',
+    )
+    return parser.parse_args()
