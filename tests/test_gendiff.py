2fcfc30a8467028883f75bed21608375dbdb76ff
Daniil <danokp@mail.ru>
2023-01-18 14:37:53 +0700

    Add test for recustion and empty dict

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,17 +11,26 @@ path_json_file1 = os.path.join(DIR_PATH, 'file1.json')
 path_json_file2 = os.path.join(DIR_PATH, 'file2.json')
 answer_file1_file2 = write_answer_from_file(DIR_PATH+'/answer_json_file1_file2.txt')
 answer_help_option = write_answer_from_file(DIR_PATH+'/answer_help_option.txt')
+path_empty_file = os.path.join(DIR_PATH, 'empty.json')
+path_json_file1_rec = os.path.join(DIR_PATH, 'file1_rec.json')
+path_json_file2_rec = os.path.join(DIR_PATH, 'file2_rec.json')
+answer_file1_file2_rec = write_answer_from_file(DIR_PATH+'/answer_json_file1_file2_rec.txt')
+
 path_yaml_file1 = os.path.join(DIR_PATH, 'file1.yaml')
 path_yaml_file2 = os.path.join(DIR_PATH, 'file2.yaml')
-
+path_yaml_file1_rec = os.path.join(DIR_PATH, 'file1_rec.yaml')
+path_yaml_file2_rec = os.path.join(DIR_PATH, 'file2_rec.yaml')
 
 
 def test_gendiff_json():
     assert generate_diff(path_json_file1, path_json_file2) == answer_file1_file2
+    assert generate_diff(path_empty_file, path_empty_file) == '{}'
+    assert generate_diff(path_json_file1_rec, path_json_file2_rec) == answer_file1_file2_rec
 
 
 def test_gendiff_yaml():
     assert generate_diff(path_yaml_file1, path_yaml_file2) == answer_file1_file2
+    assert generate_diff(path_yaml_file1_rec, path_yaml_file2_rec) == answer_file1_file2_rec
 
 
 # def test_gendiff_options():
