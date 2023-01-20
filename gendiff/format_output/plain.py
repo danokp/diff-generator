b52a5a9a42aeb10b6909c4c0bfb9c92376b15d14
Daniil <danokp@mail.ru>
2023-01-20 08:36:19 +0700

    Add format module and plain format

---
+++ gendiff/format_output/plain.py
@@ -0,0 +1,71 @@
+"""Convert comparison dictionary in plain style."""
+from gendiff.data_coparison import (
+    VALUE_ADDED,
+    VALUE_CHANGED,
+    VALUE_REMOVED,
+    VALUE_UNCHANGED,
+)
+from gendiff.format_output.stylish import get_info
+
+
+def convert_values_in_right_style(value_convert) -> str:
+    """Convert values in the right style.
+
+    Args:
+        value_convert: A value to be converted.
+
+    Returns:
+        The converted value from convert_dict. If it isn't exist, returns value.
+    """
+    convert_dct = {
+        True: 'true',
+        False: 'false',
+        None: 'null',
+    }
+    if isinstance(value_convert, str):
+        return f"'{value_convert}'"
+    elif isinstance(value_convert, dict):
+        return '[complex value]'
+    return convert_dct.get(value_convert, value_convert)
+
+
+def make_one_diff_line(key_list: list, value, meta: str) -> str:
+    diff_line = f"Property '{'.'.join(key_list)}' was "
+    if meta == VALUE_CHANGED:
+        return (
+            f'{diff_line}'
+            f'updated. From {convert_values_in_right_style(value[0])} '
+            f'to {convert_values_in_right_style(value[1])}'
+        )
+    elif meta == VALUE_ADDED:
+        return (
+            f'{diff_line}'
+            f'added with value: '
+            f'{convert_values_in_right_style(value)}'
+        )
+    elif meta == VALUE_REMOVED:
+        return f'{diff_line}removed'
+
+
+def plain(diff_dict: dict) -> str:
+    def inner(diff_dict: dict, acumulator: list) -> str:
+        diff_result = []
+        for key in sorted(diff_dict.keys()):
+            metadate = get_info(diff_dict[key], 'meta')
+            value = get_info(diff_dict[key], 'value')
+            if isinstance(value, dict):
+                if any(isinstance(v, list) for v in value.values()):
+                    diff_result.append(inner(value, acumulator + [key]))
+                else:
+                    diff_result.append(
+                        make_one_diff_line(acumulator + [key], value, metadate),
+                    )
+            else:
+                if metadate == VALUE_UNCHANGED:
+                    continue
+                else:
+                    diff_result.append(
+                        make_one_diff_line(acumulator + [key], value, metadate),
+                    )
+        return '\n'.join(diff_result)
+    return inner(diff_dict, [])
---
+++ gendiff/format_output/stylish.py
@@ -0,0 +1,153 @@
+"""Convert comparison dictionary in tree-shaped style."""
+from gendiff.data_coparison import (
+    VALUE_ADDED,
+    VALUE_CHANGED,
+    VALUE_REMOVED,
+    VALUE_UNCHANGED,
+)
+
+
+def _fill(name: str, size: int, filler: str = ' ') -> str:
+    """Convert string into string of the needed length.
+
+    Args:
+        name: A string to convert.
+        size: The result length of the string.
+        (if size <= len(name) nothing will change)
+        filler: A filler (default ' ').
+
+    Returns:
+        The string filled by filler to the length equal to size.
+    """
+    return name.rjust(size - 1, filler).ljust(size, filler)
+
+
+def convert_values_in_right_style(value_convert) -> str:
+    """Convert values in the right style.
+
+    Args:
+        value_convert: A value to be converted.
+
+    Returns:
+        The converted value from convert_dict. If it isn't exist, returns value.
+    """
+    convert_dct = {
+        True: 'true',
+        False: 'false',
+        None: 'null',
+    }
+    return convert_dct.get(value_convert, value_convert)
+
+
+def make_one_diff_line(
+    key, meta: str, value, lvl: int, indent_size: int, filler: str,
+) -> str:
+    """Create one difference line.
+
+    Args:
+        key: A dictionary key.
+        meta: Comparison result.
+        value: A dictionary value.
+        lvl: Depth of comparison.
+        indent_size: The size of indent.
+        filler: A symble indent is filled with.
+
+    Returns:
+        One difference line.
+    """
+    diff_meaning_dict = {
+        VALUE_UNCHANGED: '',
+        VALUE_ADDED: '+',
+        VALUE_REMOVED: '-',
+    }
+    return (
+        f'{lvl * indent_size * filler}'
+        f'{_fill(diff_meaning_dict[meta], size=indent_size)}'
+        f'{key}'
+        f'{":" if value == "" else ": "}'
+        f'{value}'
+    )
+
+
+def get_info(item, info_type: str):
+    """Get value or metadate from diff dictionary.
+
+    Args:
+        item: The item to get information from.
+        info_type: The information type (metadate or value).
+
+    Returns:
+        Value or metadate from diff dictionary
+    """
+    d = {
+        'meta': (1, VALUE_UNCHANGED),
+        'value': (0, item),
+    }
+    index, returned_item = d[info_type]
+    return (
+        item[index] if isinstance(item, list)
+        else returned_item
+    )
+
+
+def stylish(diff_dict: dict) -> str:
+    """Stylish difference dictionary.
+
+    Args:
+         diff_dict: A difference dictionary.
+
+    Returns:
+        Styled string.
+    """
+    def inner(
+        diff_dict: dict,
+        lvl: int,
+        indent_size: int = 4,
+        filler: str = ' ',
+    ) -> str:
+        diff_result = ['{']
+        for key in sorted(diff_dict.keys()):
+            metadate = get_info(diff_dict[key], 'meta')
+            value = get_info(diff_dict[key], 'value')
+            if isinstance(value, dict):
+                diff_result.append(
+                    make_one_diff_line(
+                        key,
+                        metadate,
+                        inner(value, lvl=lvl + 1),
+                        lvl,
+                        indent_size,
+                        filler,
+                    ),
+                )
+            else:
+                if metadate == VALUE_CHANGED:
+                    for meta, i in zip((VALUE_REMOVED, VALUE_ADDED), (0, 1)):
+                        diff_result.append(
+                            make_one_diff_line(
+                                key,
+                                meta,
+                                inner(value[i], lvl=lvl + 1)
+                                if isinstance(value[i], dict)
+                                else convert_values_in_right_style(value[i]),
+                                lvl,
+                                indent_size,
+                                filler,
+                            ),
+                        )
+                else:
+                    diff_result.append(
+                        make_one_diff_line(
+                            key,
+                            metadate,
+                            convert_values_in_right_style(value),
+                            lvl,
+                            indent_size,
+                            filler,
+                        ),
+                    )
+        diff_result.append(f'{lvl * indent_size * filler}}}')
+        return '\n'.join(diff_result)
+    if diff_dict == {}:
+        return '{}'
+    return inner(diff_dict, lvl=0)
