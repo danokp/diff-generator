901ddbcea399999fee5dcf562f647cd55d49eb5b
Daniil <danokp@mail.ru>
2023-01-21 12:52:10 +0700

    Update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,35 +7,43 @@ def write_answer_from_file(path):
     with open(path) as file:
         return ''.join(file.readlines())
 
+
+def make_path_to_file(file_name):
+    return os.path.join(DIR_PATH, file_name)
+
+
 DIR_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
-path_json_file1 = os.path.join(DIR_PATH, 'file1.json')
-path_json_file2 = os.path.join(DIR_PATH, 'file2.json')
-path_empty_file = os.path.join(DIR_PATH, 'empty.json')
-path_json_file1_rec = os.path.join(DIR_PATH, 'file1_rec.json')
-path_json_file2_rec = os.path.join(DIR_PATH, 'file2_rec.json')
-path_yaml_file1 = os.path.join(DIR_PATH, 'file1.yaml')
-path_yaml_file2 = os.path.join(DIR_PATH, 'file2.yaml')
-path_yaml_file1_rec = os.path.join(DIR_PATH, 'file1_rec.yaml')
-path_yaml_file2_rec = os.path.join(DIR_PATH, 'file2_rec.yaml')
 
-answer_stylish_file1_file2_rec = write_answer_from_file(DIR_PATH+'/answer_json_file1_file2_rec.txt')
-answer_plain_file1_file2_rec = write_answer_from_file(DIR_PATH+'/answer_plain_file1_file2_rec.txt')
-answer_file1_file2 = write_answer_from_file(DIR_PATH+'/answer_json_file1_file2.txt')
-answer_help_option = write_answer_from_file(DIR_PATH+'/answer_help_option.txt')
+json_file1 = 'file1.json'
+json_file2 = 'file2.json'
+empty_file = 'empty.json'
+json_file1_rec = 'file1_rec.json'
+json_file2_rec = 'file2_rec.json'
+yaml_file1 = 'file1.yaml'
+yaml_file2 = 'file2.yaml'
+yaml_file1_rec = 'file1_rec.yaml'
+yaml_file2_rec = 'file2_rec.yaml'
 
+answer_stylish_file1_file2_rec = 'answer_stylish_file1_file2_rec.txt'
+answer_plain_file1_file2_rec = 'answer_plain_file1_file2_rec.txt'
+answer_json_file1_file2_rec = 'answer_json_file1_file2_rec.txt'
+answer_file1_file2 = 'answer_json_file1_file2.txt'
+answer_empty = 'answer_empty.txt'
+
+print(make_path_to_file(yaml_file2_rec))
+print(make_path_to_file(answer_stylish_file1_file2_rec))
 @pytest.mark.parametrize('first_file, second_file, answer, format',[
-    (path_empty_file, path_empty_file, '{}', 'stylish'),
-    (path_yaml_file1, path_yaml_file2, answer_file1_file2, 'stylish'),
-    (path_json_file1, path_json_file2, answer_file1_file2, 'stylish'),
-    (path_yaml_file1_rec, path_yaml_file2_rec, answer_stylish_file1_file2_rec, 'stylish'),
-    (path_json_file1_rec, path_json_file2_rec, answer_stylish_file1_file2_rec, 'stylish'),
-    (path_yaml_file1_rec, path_yaml_file2_rec, answer_plain_file1_file2_rec, 'plain'),
-    (path_json_file1_rec, path_json_file2_rec, answer_plain_file1_file2_rec, 'plain'),
+    (empty_file, empty_file, answer_empty, 'stylish'),
+    (yaml_file1, yaml_file2, answer_file1_file2, 'stylish'),
+    (json_file1, json_file2, answer_file1_file2, 'stylish'),
+    (yaml_file1_rec, yaml_file2_rec, answer_stylish_file1_file2_rec, 'stylish'),
+    (json_file1_rec, json_file2_rec, answer_stylish_file1_file2_rec, 'stylish'),
+    (yaml_file1_rec, yaml_file2_rec, answer_plain_file1_file2_rec, 'plain'),
+    (json_file1_rec, json_file2_rec, answer_plain_file1_file2_rec, 'plain'),
+    (json_file1_rec, json_file2_rec, answer_json_file1_file2_rec, 'json'),
+    (yaml_file1_rec, yaml_file2_rec, answer_json_file1_file2_rec, 'json'),
 ])
 def test_gendiff(first_file, second_file, format, answer):
-    assert generate_diff(first_file, second_file, format) == answer
-
-
-
-# def test_gendiff_options():
-#     assert == answer_help_option
+    assert generate_diff(
+        make_path_to_file(first_file), make_path_to_file(second_file), format
+    ) == write_answer_from_file(make_path_to_file(answer))
