1e4f41915faf0ef7e63670c14c2c76423d50facf
Daniil <danokp@mail.ru>
2023-01-25 00:49:57 +0700

    Change return to print in gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,10 +5,12 @@ from gendiff.generate_diff import generate_diff
 
 def main():
     inputs = parse_args()
-    return generate_diff(
-        inputs.first_file,
-        inputs.second_file,
-        inputs.format,
+    print(
+        generate_diff(
+            inputs.first_file,
+            inputs.second_file,
+            inputs.format,
+        )
     )
 
 
