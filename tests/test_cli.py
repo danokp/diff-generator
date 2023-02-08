a43cef1b36c374463796f0fb6daa78ad535165f3
Daniil <danokp@mail.ru>
2023-02-08 17:54:30 +0700

    Update tests

---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+from gendiff.cli import parse_args
+from tests.test_gendiff import test_formats, test_files
+
+
+def test_parse_args(test_formats, test_files):
+    first_file, second_file = test_files
+    format = test_formats[0]
+    sys.argv = [
+        'gendiff',
+        first_file,
+        second_file, 
+        '-f',
+        format
+    ]
+    args = parse_args()
+    assert args.first_file == first_file
+    assert args.second_file == second_file
+    assert args.format == format
+
+
+def test_cli_launch():
+    a = os.system('poetry run gendiff -h')
+    assert a == 0
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,14 @@
-from gendiff.generate_diff import generate_diff
 import os
 import pytest
+
+
+from gendiff.generate_diff import generate_diff
 
 
 DIR_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
 TEST_FILES = {
     'json': ['file1_rec.json', 'file2_rec.json'],
     'yaml': ['file1_rec.yaml', 'file2_rec.yaml'],
-    'yaml_json': ['file1_rec.yaml', 'file2_rec.json']
 }
 TEST_FORMATS = {
     'stylish': 'answer_stylish_file1_file2_rec.txt',
@@ -55,9 +56,22 @@ def test_gendiff_empty():
 
 def test_gendiff_exception(test_files, test_wrong_formats):
     first_file, second_file = test_files
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as exception:
         generate_diff(
             make_path_to_file(first_file),
             make_path_to_file(second_file),
             test_wrong_formats
         )
+    assert str(exception.value) == "Choose one of the available formats: " \
+                                   "['stylish', 'plain', 'json']."
+
+
+def test_parse_exception():
+    first_file = 'wrong_file_format.txt'
+    second_file = 'wrong_file_format.txt'
+    with pytest.raises(ValueError) as exception:
+        generate_diff(
+            make_path_to_file(first_file),
+            make_path_to_file(second_file),
+        )
+    assert str(exception.value) == 'Only json, yaml or yml formats are available.'
